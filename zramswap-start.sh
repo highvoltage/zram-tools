f44118f1eeb918b61deba050db7019c54104c6b8
Jonathan Carter <jcarter@linux.com>
2014-10-05 15:43:29 +0200

    Update zramswap-start.sh

--- zramswap-start.sh
+++ zramswap-start.sh
@@ -6,16 +6,25 @@
 #  * Space is assigned to each zram device, then swap is initialized on
 #    there
 
-ASSIGNED_ZRAM_SWAP=1000 # Swap you want assigned in MiB
-ASSIGNED_ZRAM_SWAP=$((ASSIGNED_ZRAM_SWAP * 1000 * 1000)) #turn amount into MiB
+# Some defaults:
+ALLOCATION=100 # ZRAM Swap you want assigned, in MiB
+PRIORITY=100   # Swap priority, see swapon(2) for more details
 
 # Get amount of available CPU cores, set to 1 if not detected correctly
 if [ ! -f /proc/cpuinfo ]; then
-    echo "ERROR: Can't find /proc/cpuinfo, is proc mounted?"
-    exit 1
+    echo "WARNING: Can't find /proc/cpuinfo, is proc mounted?"
+    echo "         Using a single core for zramswap..."
+    cores=1
 else
     cores=$(grep -c processor /proc/cpuinfo)
 fi
+
+# Override  above from config file, if it exists
+if [ -f /etc/default/zramswap ]; then
+    . /etc/default/zramswap
+fi
+
+ALLOCATION=$((ALLOCATION * 1000 * 1000)) #turn amount into MiB
 
 # Initialize zram devices, one device per CPU core
 modprobe zram num_devices=$cores
@@ -23,7 +32,7 @@ modprobe zram num_devices=$cores
 # Assign memory to zram devices, initialize swap and activate
 # Decrementing $core, because cores start counting at 0
 for core in $(seq 0 $(($cores - 1))); do
-    echo $(($ASSIGNED_ZRAM_SWAP / $cores)) > /sys/block/zram$core/disksize
+    echo $(($ALLOCATION / $cores)) > /sys/block/zram$core/disksize
     mkswap /dev/zram$core
-    swapon -p 100 /dev/zram$core
+    swapon -p $PRIORITY /dev/zram$core
 done
