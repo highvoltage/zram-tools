0c8f3f976b71a77ecb9d4a03fae9123bb413cb9a
Jonathan Carter <jonathan@bluemosh.com>
2015-10-19 10:30:32 +0200

    Apply patches from @IvanTomica

--- zramswap.sh
+++ zramswap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script does the following:
 # zramswap start:
-#  * zram doesn't natively support multiple CPU cores, so one zram device
-#    is created for every CPU core minus one)
+#  * Create one ZRAM device and activate max_comp_stream accordingly to
+#    number of CPUs on the system
 #  * Space is assigned to each zram device, then swap is initialized on
 #    there
 # zramswap stop:
@@ -10,16 +10,16 @@
 
 function start {
     #Set some defaults:
-    ALLOCATION=100 # ZRAM Swap you want assigned, in MiB
+    ALLOCATION=256 # ZRAM Swap you want assigned, in MiB
     PRIORITY=100   # Swap priority, see swapon(2) for more details
 
     # Get amount of available CPU cores, set to 1 if not detected correctly
     if [ ! -f /proc/cpuinfo ]; then
         echo "WARNING: Can't find /proc/cpuinfo, is proc mounted?"
         echo "         Using a single core for zramswap..."
-        cores=1
+        CORES=1
     else
-        cores=$(grep -c processor /proc/cpuinfo)
+        CORES=$(grep -c processor /proc/cpuinfo)
     fi
 
     # Override  above from config file, if it exists
@@ -31,25 +31,23 @@ function start {
 
     if [ -n "$PERCENTAGE" ]; then
         totalmemory=$(awk '/MemTotal/{print $2}' /proc/meminfo) # in KiB
-        ALLOCATION=$((totalmemory * 1024 * $PERCENTAGE / 100))
+        ALLOCATION=$((totalmemory * 1024 * PERCENTAGE / 100))
     fi
 
     # Initialize zram devices, one device per CPU core
-    modprobe zram num_devices=$cores
+    modprobe zram num_devices=1
+        # Use multiple cores
+        echo "$CORES" > /sys/block/zram0/max_comp_streams
+        echo "$ALLOCATION" > /sys/block/zram0/disksize
+        mkswap /dev/zram0
+        swapon -p "$PRIORITY" /dev/zram0
 
-    # Assign memory to zram devices, initialize swap and activate
-    # Decrementing $core, because cores start counting at 0
-    for core in $(seq 0 $(($cores - 1))); do
-        echo $(($ALLOCATION / $cores)) > /sys/block/zram$core/disksize
-        mkswap /dev/zram$core
-        swapon -p $PRIORITY /dev/zram$core
-    done
 }
 
 function status {
     for f in /sys/block/zram*/*_data_size ; do
-        read size < $f
-        what=$(basename $f)
+        read -r size < "$f"
+        what=$(basename "$f")
         eval "$what=\$(($what + $size))"
     done
     echo "compr_data_size: $((compr_data_size / 1024)) KiB"
@@ -59,7 +57,7 @@ function status {
 
 function stop {
     for swapspace in $(swapon -s | awk '/zram/{print $1}'); do
-        swapoff $swapspace
+        swapoff "$swapspace"
     done
     modprobe -r zram
 }
@@ -73,16 +71,11 @@ function usage {
 
 if [ "$1" = "start" ]; then
     start
-fi
-
-if [ "$1" = "stop" ]; then
+elif [ "$1" = "stop" ]; then
     stop
-fi
-
-if [ "$1" = "status" ]; then
+elif [ "$1" = "status" ]; then
     status
-fi
-
-if [ "$1" = "" ]; then
+elif [ "$1" = "" ]; then
     usage
+else echo "unknown option: $1";
 fi
